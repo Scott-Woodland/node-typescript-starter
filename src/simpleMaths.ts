3305a7343a3b070e614d9c327ac6fc6e1d5624d6
Scott Woodland <scott@wenet.com>
2021-11-18 09:16:43 +0000

    Circle Area Calculation Added

--- src/simpleMaths.ts
+++ src/simpleMaths.ts
@@ -1,8 +1,13 @@
+const pi = 3.141592;
 export function calcAreaSquare(sideLength: number): number {
     return Math.pow(sideLength, 2);
 }
 
 export function calcAreaTriangle(width: number, height: number): number {
     return width * height / 2;
+}
+
+export function calcAreaCircle(radius: number): number {
+    return radius * pi;
 }
 
